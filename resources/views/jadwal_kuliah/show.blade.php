529859365a495cae44938d7c064e258ebe719efb
SunnFlower47 <ridwanandrian2004@gmail.com>
2025-05-04 17:17:13 +0700

    pernaikan di views

--- resources/views/jadwal_kuliah/show.blade.php
+++ resources/views/jadwal_kuliah/show.blade.php
@@ -5,15 +5,47 @@
     <h3>Detail Jadwal</h3>
     <div class="card">
         <div class="card-body">
-            <p><strong>Kode MK:</strong> {{ $jadwal->kode_mk }}</p>
-            <p><strong>Nama MK:</strong> {{ $jadwal->nama_mk }}</p>
-            <p><strong>Jurusan:</strong> {{ $jadwal->jurusan }}</p>
-            <p><strong>Tahun Akademik:</strong> {{ $jadwal->tahun_akademik }}</p>
-            <p><strong>Semester:</strong> {{ $jadwal->semester }}</p>
-            <p><strong>Dosen:</strong> {{ $jadwal->nama_dosen }}</p>
-            <p><strong>Ruang:</strong> {{ $jadwal->ruang }}</p>
-            <p><strong>Hari:</strong> {{ $jadwal->hari }}</p>
-            <p><strong>Jam:</strong> {{ \Carbon\Carbon::parse($jadwal->jam_mulai)->format('H:i') }} - {{ \Carbon\Carbon::parse($jadwal->jam_selesai)->format('H:i') }}</p>
+            <table class="table table-bordered">
+                <tr>
+                    <th>Kode MK</th>
+                    <td>{{ $jadwal->kode_mk }}</td>
+                </tr>
+                <tr>
+                    <th>Nama MK</th>
+                    <td>{{ $jadwal->nama_mk }}</td>
+                </tr>
+                <tr>
+                    <th>Jurusan</th>
+                    <td>{{ $jadwal->jurusan }}</td>
+                </tr>
+                <tr>
+                    <th>Tahun Akademik</th>
+                    <td>{{ $jadwal->tahun_akademik }}</td>
+                </tr>
+                <tr>
+                    <th>Semester</th>
+                    <td>{{ ucfirst($jadwal->semester) }}</td>
+                </tr>
+                <tr>
+                    <th>Dosen</th>
+                    <td>{{ $jadwal->nama_dosen }}</td>
+                </tr>
+                <tr>
+                    <th>Ruang</th>
+                    <td>{{ $jadwal->ruang }}</td>
+                </tr>
+                <tr>
+                    <th>Hari</th>
+                    <td>{{ $jadwal->hari }}</td>
+                </tr>
+                <tr>
+                    <th>Jam</th>
+                    <td>
+                        {{ \Carbon\Carbon::parse($jadwal->jam_mulai)->format('H:i') }} -
+                        {{ \Carbon\Carbon::parse($jadwal->jam_selesai)->format('H:i') }}
+                    </td>
+                </tr>
+            </table>
         </div>
     </div>
     <a href="{{ route('jadwal_kuliah.index') }}" class="btn btn-secondary mt-3">Back</a>
